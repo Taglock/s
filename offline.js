c5206eb975451c38ff68bb72d60504ea40bed65b
Taglock <39584594+Taglock@users.noreply.github.com>
2022-07-17 10:51:46 +0500

    s

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1658036738,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"img/aimspot-sheet0.png",
+		"img/peer-sheet0.png",
+		"img/peerlaser-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
